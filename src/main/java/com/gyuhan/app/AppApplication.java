386869512ef57ad301b6a30a13ca697765fe0885
lns13301 <rkdrbgks22@ks.ac.kr>
2020-04-08 14:04:25 +0900

    Initial commit
    VCS을 위한 업로드

---
+++ src/main/java/com/gyuhan/app/AppApplication.java
@@ -0,0 +1,13 @@
+package com.gyuhan.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
