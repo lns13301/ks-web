00f96fc233017b2e26bef43915e8991ec082395d
lns13301 <rkdrbgks22@ks.ac.kr>
2020-04-27 14:11:05 +0900

    feat: added dummy data

--- src/main/java/com/gyuhan/app/AppApplication.java
+++ src/main/java/com/gyuhan/app/AppApplication.java
@@ -1,13 +1,27 @@
 package com.gyuhan.app;
 
+import com.gyuhan.app.entity.Person;
+import com.gyuhan.app.repository.PersonRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AppApplication {
+public class AppApplication implements CommandLineRunner {
+
+	private final PersonRepository personRepository;
+
+	public AppApplication(PersonRepository personRepository) {
+		this.personRepository = personRepository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
 	}
 
+
+	@Override
+	public void run(String... args) throws Exception {
+		personRepository.save(new Person("강규한", "백엔드", "hello@test.com", "010-1234-5678"));
+	}
 }
