de22f1d92df10d672b1b04a4c63a06a2049012b8
lns13301 <rkdrbgks22@ks.ac.kr>
2020-04-27 14:04:53 +0900

    feat: added Person class
    - 생성자
    - Getter, Setter

---
+++ src/main/java/com/gyuhan/app/entity/Person.java
@@ -0,0 +1,61 @@
+package com.gyuhan.app.entity;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+public class Person {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String name;
+    private String job;
+    private String email;
+    private String phoneNo;
+
+    public Person(String name, String job, String email, String phoneNo) {
+        this.name = name;
+        this.job = job;
+        this.email = email;
+        this.phoneNo = phoneNo;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+}
