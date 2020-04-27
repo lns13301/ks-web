ebdf8f201941a83bb1c0e222df58284eb143f8cb
lns13301 <rkdrbgks22@ks.ac.kr>
2020-04-27 14:13:04 +0900

    feat: connect Person object to View

--- src/main/java/com/gyuhan/app/controller/IndexController.java
+++ src/main/java/com/gyuhan/app/controller/IndexController.java
@@ -1,10 +1,18 @@
 package com.gyuhan.app.controller;
 
+import com.gyuhan.app.repository.PersonRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
+
+    private final PersonRepository personRepository;
+
+    public IndexController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
     @GetMapping("/")
     public String index() {
         return "resume";
